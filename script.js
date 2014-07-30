8d58937b1c7e74dc49cd75e0752345a37dce45a9
Mallory Busch <M.MalloryBusch@gmail.com>
2014-07-29 22:19:01 -0500

    random

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 
 
-$(document).ready(function(){	
-
-	$("#animate-button" ).click(startAnimation);
-
-	function startAnimation(){
-		$(".box").animate({
-    		opacity: 0.5,
-    		//backgroundColor:"red",
-    		left: "+=500",
-    		height: "toggle",
-  		}, 5000, 
-  		function() {
-    			console.log("animation complete");
-  		});
-	}
-	
-///////////PART TWO////////////////
-
-	//$( ".box" ).addClass( "red", 1000);
-		//removeClass also works here
-	//$( ".box" ).effect("explode", 1000);
-		//Try also blind, bounce, clip, drop, fade, fold, highlight, puff, pulsate, scale, shake, size, slide, or transfer
-		//Effects can also be used in conjunction with the "hide", "show", "toggle", or "toggleClass" commands. --> .hide("puff",1000); 
-
-
-
-
-//end doc ready
+function interval() {
+  $('.box').effect('shake', { times:3 }, 100);
+}
+$(document).ready(function() {  
+  var shake = setInterval(interval, 5000);    
 });
+
+// $(document).ready(function(){	
+
+	
+// ///////////PART TWO////////////////
+
+// 	//$( ".box" ).addClass( "red", 1000);
+// 		//removeClass also works here
+// 	$( ".box" ).effect("shake", 
+// 			{times:10}, 1000);
+// 		//Try also blind, bounce, clip, drop, fade, fold, highlight, puff, pulsate, scale, shake, size, slide, or transfer
+// 		//Effects can also be used in conjunction with the "hide", "show", "toggle", or "toggleClass" commands. --> .hide("puff",1000); 
+
+
+// // $( ".box" ).effect("shake", 
+// // 			{times:10}, 1000);
+
+// //end doc ready
+// });
